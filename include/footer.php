20c98b318b55ea65b46ad65d1c35f5a96d82b11d
eastonsmith <eastonsmith37@gmail.com>
2022-04-01 18:14:51 -0400

    updatedfooter

--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,8 @@
     <footer class="py-3 my-4">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         </ul>
-        <p class="text-center text-light">© 2021 Company, Inc</p>
+
+        <p class="text-center text-light">© 2022 Group 4; Easton Smith, Jordan _, (other members)</p>
     </footer>
 </div>
 </body>
